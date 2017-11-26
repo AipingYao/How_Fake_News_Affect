712fcefe85e9c38f94dbdca72ec45cb4ea0c6fed
mablatter <mario.blatter@alumni.ethz.ch>
2017-11-26 13:26:13 +0100

    Fixed shrinking connection matrix bug

--- code/Without_fake_news.m
+++ code/Without_fake_news.m
@@ -7,21 +7,20 @@ N=3200; % no of people
 k=2*M/N; % avg degree
 gamma=20; % N/G
 G=N/gamma; % number of opinion
-phi=0.98; % transition probability
+phi=0.04; % transition probability
 
 
-no_of_runs= 1; % amount of times to run simulation
+no_of_runs= 10; % amount of times to run simulation
 duration= 100000; % number of iterations
 %------------- ENDCONFIG -----------------------
 
 ClusterSizes = opinion_change_model(N,M,k,G,phi,no_of_runs,duration);
 
-
 for r = 1:2:no_of_runs
     ClusterCount = squeeze(ClusterSizes(:,r));
     for s = 1:2*max(ClusterCount)
         p(s,r) = size(find(ClusterCount(:) == s),1);
-        P(s,r) = p(s,r)./N;   %The possiblity of the group size;
+        P(s,r) = p(s,r)./N;   %The probability of the group size;
     end
     
 end
