ca45aaaeade79b6ad33ce3f0c5aa8760f0d28381
mablatter <mario.blatter@alumni.ethz.ch>
2017-11-29 14:53:38 +0100

    Added script to replicate all three plots in Holme

---
+++ code/paper_replication.m
@@ -0,0 +1,38 @@
+%------------Initialise 1st model
+clear all
+
+%------------- CONFIG -------------------------
+M=6400; % no of connections
+N=3200; % no of people
+k=2*M/N; % avg degree
+gamma=10; % N/G
+G=N/gamma; % number of opinion
+phi_1=0.04; % transition probability
+
+no_of_runs= 1000; % amount of times to run simulation
+duration= 1000000; % number of iterations within each run
+%------------- ENDCONFIG -----------------------
+
+ClusterSizes_1 = opinion_change_model(N,M,k,G,phi_1,no_of_runs,duration);
+
+[s_averaged_binned_1, s_averaged_1] = plot_averaged_results(ClusterSizes_1,N,M,G,phi_1,duration,no_of_runs);
+
+%------------Initialise 2nd model
+
+%------------- CONFIG -------------------------
+phi_2=0.458; % transition probability
+%------------- ENDCONFIG -----------------------
+
+ClusterSizes_2 = opinion_change_model(N,M,k,G,phi_2,no_of_runs,duration);
+
+[s_averaged_binned_2, s_averaged_2] = plot_averaged_results(ClusterSizes_2,N,M,G,phi_2,duration,no_of_runs);
+
+%------------Initialise 3rd model
+
+%------------- CONFIG -------------------------
+phi_3=0.96; % transition probability
+%------------- ENDCONFIG -----------------------
+
+ClusterSizes_3 = opinion_change_model(N,M,k,G,phi_3,no_of_runs,duration);
+
+[s_averaged_binned_3, s_averaged_3] = plot_averaged_results(ClusterSizes_3,N,M,G,phi_3,duration,no_of_runs);
