3d0dc37a7c8764bd1e1a935d562284acdec87bda
mablatter <mario.blatter@alumni.ethz.ch>
2017-12-13 16:02:09 +0100

    Reorganized file structure, added plots and data

--- code/Without_fake_news.m
+++ code/Without_fake_news.m
@@ -8,6 +8,7 @@ k=2*M/N; % avg degree
 gamma=10; % N/G
 G=N/gamma; % number of opinion
 phi=0.04; % transition probability
+Fake.add = 0;
 
 no_of_runs= 3; % amount of times to run simulation
 abort_threshold = 1000; % number of times clusters have to stay the same
@@ -19,4 +20,4 @@ duration= 1000000; % number of iterations within each run
 
 [bla, s_averaged] = ...
     plot_averaged_results(ClusterSizes,N,M,G,phi,duration,...
-    average_iterations,no_of_runs);
+    average_iterations,no_of_runs,Fake);
--- code/paper_replication.m
+++ code/paper_replication.m
@@ -2,38 +2,46 @@
 clear all
 
 %------------- CONFIG -------------------------
-M=60; % no of connections
-N=30; % no of people
+M=6400; % no of connections
+N=3200; % no of people
 k=2*M/N; % avg degree
 gamma=10; % N/G
 G=N/gamma; % number of sopinion
 phi_1=0.04; % transition probability
+Fake.add=0;
 
-no_of_runs= 10; % amount of times to run simulation
-abort_threshold = 1000;
+no_of_runs= 20; % amount of times to run simulation
+abort_threshold = 10000;
 duration= 1000000; % number of iterations within each run
 %------------- ENDCONFIG -----------------------
 
-ClusterSizes_1 = opinion_change_model(N,M,k,G,phi_1,no_of_runs,duration);
+[ClusterSizes_1,average_iterations_1] = ...
+    opinion_change_model(N,M,k,G,phi_1,no_of_runs,duration,abort_threshold);
 % 
-[s_averaged_binned_1, s_averaged_1] = plot_averaged_results(ClusterSizes_1,N,M,G,phi_1,duration,no_of_runs);
+%[s_averaged_binned_1, s_averaged_1] = ...
+%    plot_averaged_results(ClusterSizes_1,N,M,G,phi_1,duration,average_iterations_1,no_of_runs,Fake);
 
+ save('20_runs 004')
 %------------Initialise 2nd model
 
 %------------- CONFIG -------------------------
-phi_2=0.458; % transition probability
-%------------- ENDCONFIG -----------------------
-
-ClusterSizes_2 = opinion_change_model(N,M,k,G,phi_2,no_of_runs,duration);
+%  phi_2=0.458; % transition probability
+% %------------- ENDCONFIG -----------------------
 % 
-[s_averaged_binned_2, s_averaged_2] = plot_averaged_results(ClusterSizes_2,N,M,G,phi_2,duration,no_of_runs);
-
-%------------Initialise 3rd model
-
-%------------- CONFIG -------------------------
-phi_3=0.96; % transition probability
-%------------- ENDCONFIG -----------------------
-
-ClusterSizes_3 = opinion_change_model(N,M,k,G,phi_3,no_of_runs,duration);
-
-[s_averaged_binned_3, s_averaged_3] = plot_averaged_results(ClusterSizes_3,N,M,G,phi_3,duration,no_of_runs);
+% [ClusterSizes_2,average_iterations_2] = ...
+%     opinion_change_model(N,M,k,G,phi_2,no_of_runs,duration,abort_threshold);% 
+% [s_averaged_binned_2, s_averaged_2] = ...
+%     plot_averaged_results(ClusterSizes_2,N,M,G,phi_2,duration,average_iterations_2,no_of_runs,Fake);
+% 
+% %save('100 runs 0458')
+% %------------Initialise 3rd model
+% 
+% %------------- CONFIG -------------------------
+% % phi_3=0.96; % transition probability
+% %------------- ENDCONFIG -----------------------
+% 
+% % [ClusterSizes_3,average_iterations_3] = ...
+% %     opinion_change_model(N,M,k,G,phi_3,no_of_runs,duration,abort_threshold);
+% % [s_averaged_binned_3, s_averaged_3] = ...
+% %    plot_averaged_results(ClusterSizes_3,N,M,G,phi_3,duration,average_iterations_3,no_of_runs,Fake);
+% save('50 runs 0458')
